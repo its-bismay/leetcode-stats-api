de899f31e51226b407d425b8f0d80e4303aec1e4
its-bismay <2023ugee070@nitjsr.ac.in>
2026-05-05 12:08:41 +0530

    V1 api CORS fixed for users

--- server.py
+++ server.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from app.config import settings
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.router import router as api_router
 
 app = FastAPI(
     title=settings.APP_NAME,
     version="1.0.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router)
